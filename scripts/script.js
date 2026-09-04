fb0c85b4a4ab8c690c438cba657c7ae1df5e9abe
Anastasiia Ilhan <nas106013@gmail.com>
2026-09-04 21:21:23 +0300

    Update priject

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const banner = document.querySelector(".cookie-banner");
+  const acceptBtn = document.querySelector(".btn-accept");
+  const rejectBtn = document.querySelector(".btn-reject");
+  const closeBanner = (event) => {
+    event.preventDefault();
+    banner.classList.add("cookie-hidden");
+  };
 
+  if (banner && acceptBtn && rejectBtn) {
+    acceptBtn.addEventListener("click", closeBanner);
+    rejectBtn.addEventListener("click", closeBanner);
+  }
+});
